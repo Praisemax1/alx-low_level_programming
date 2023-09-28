04dd05dfaa2c7c87089bdd1ba78973ad4290b19a
Praisemax1 <praisegodosede@gmail.com>
2023-09-28 08:11:47 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -5,17 +5,14 @@
  *
  * Return: The length of the string.
  */
-
 int _strlen_recursion(char *s)
-
 {
 	int longit = 0;
-	if (*s)
 
+	if (*s)
 	{
 		longit++;
 		longit += _strlen_recursion(s + 1);
 	}
-
 	return (longit);
 }
