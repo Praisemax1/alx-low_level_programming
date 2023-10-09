380e36c4f5dd4154bb48a1717124afc2d383b69e
Praisemax1 <praisegodosede@gmail.com>
2023-10-08 17:30:59 -0700

    0x0C. C - More malloc, free

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,72 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Function declarations */
+int is_digit(char *s);
+int _strlen(char *s);
+void errors(void);
+int *multiply(char *s1, char *s2);
+void print_result(int *result, int len);
+
+/* Function definitions */
+
+/**
+ * is_digit - checks if a string contains only digits
+ * @s: string to be evaluated
+ * Return: 1 if all characters are digits, 0 otherwise
+ */
+int is_digit(char *s)
+{
+	/* ... */
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	/* ... */
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	/* ... */
+}
+
+/**
+ * multiply - multiplies two numbers represented as strings
+ * @s1: first number as a string
+ * @s2: second number as a string
+ * Return: pointer to an array containing the result
+ */
+int *multiply(char *s1, char *s2)
+{
+	/* ... */
+}
+
+/**
+ * print_result - prints the result of multiplication
+ * @result: pointer to the array containing the result
+ * @len: length of the result array
+ */
+void print_result(int *result, int len)
+{
+	/* ... */
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	/* ... */
+}
